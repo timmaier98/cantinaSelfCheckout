cd67baeccf6c0921cee4322c1e1d916952d8aa17
timmaier98 <54843889+timmaier98@users.noreply.github.com>
2022-12-08 16:07:13 +0100

    changed to tkinter

--- gui.py
+++ gui.py
@@ -1,29 +1,35 @@
-import tkinter
-import customtkinter as ctk
-
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 
 class Gui:
 
     def __init__(self):
 
-        Font_Buttons = ("Roman", 36)
+        syscolor = "#031c29"
+        syscolorButtons = "#3a3a3a"
+        Font_Buttons = ("Roboto", 36)
         custom_green = "#2FA572"
         custom_red = "#A52F2F"
 
-        ctk.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
-        ctk.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+        #tk.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
+        #tk.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
-        self.app = ctk.CTk()
+        self.app = tk.Tk()
         self.app.geometry("1024x600")
         self.app.resizable(False, False)
         self.app.title("cantinaSelfCheckout")
+        self.app.config(background=syscolor)  # Hintergrundfarbe des Fensters
 
-        self.frame_base = ctk.CTkFrame(master=self.app)
+        s = ttk.Style()
+        s.configure('Custom.TFrame', background=syscolor)
+
+        self.frame_base = ttk.Frame(master=self.app, style='Custom.TFrame')
         self.frame_base.pack(pady=20, padx=20, fill="both", expand=True)
 
-        btn = ctk.CTkButton(master=self.frame_base, text='Confirm', width=200, height=100, text_color="white",
-                            font=Font_Buttons, fg_color=custom_green)
-        btn.place(relx=0.99, rely=0.99, anchor=tkinter.SE)
+        btn = tk.Button(master=self.frame_base, text='Confirm', width=8, height=2,
+                         font=Font_Buttons)
+        btn.place(relx=0.99, rely=0.99, anchor=tk.SE)
 
         self.app.mainloop()
 
