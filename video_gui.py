2521fdc42ed7538ca35454b7d1e39b4bf2467742
Fabian <fabian.krauss77@gmail.com>
2022-12-08 16:35:44 +0100

    Added some documentation to video-gui.py

--- video_gui.py
+++ video_gui.py
@@ -5,9 +5,11 @@ import cv2
 
 
 class VideoGUI:
+    """ Tkinter GUI class, which demonstrates a live video feed using OpenCV and the built-in webcam. """
+
     def __init__(self):
         root = tkinter.Tk()
-        root.title("Example about handy CV2 and tkinter combination...")
+        root.title("cantinaSelfCheckout")
 
         self.frame = np.random.randint(0, 255, [100, 100, 3], dtype='uint8')
         # self.img = ImageTk.PhotoImage(Image.fromarray(self.frame))
@@ -21,23 +23,24 @@ class VideoGUI:
         text_label = tkinter.Label(root, text=message)
         text_label.grid(row=1, column=1, pady=1, padx=10)
 
-        button1 = tkinter.Button(root, text="Start", command=self.start, height=5, width=20)
-        button1.grid(row=1, column=0, pady=10, padx=10)
-        button1.config(height=10, width=20)
+        start_btn = tkinter.Button(root, text="Start", command=self.start, height=5, width=20)
+        start_btn.grid(row=1, column=0, pady=10, padx=10)
+        start_btn.config(height=10, width=20)
 
-        button2 = tkinter.Button(root, text="Stop", command=self.stop, height=5, width=20)
-        button2.grid(row=1, column=2, pady=10, padx=10)
-        button2.config(height=10, width=20)
+        stop_btn = tkinter.Button(root, text="Stop", command=self.stop, height=5, width=20)
+        stop_btn.grid(row=1, column=2, pady=10, padx=10)
+        stop_btn.config(height=10, width=20)
 
         root.mainloop()
 
-    def start(self):
-        self.cam = cv2.VideoCapture(0)
+    def start(self, cam_number=0):
+        """ Start the video feed with a given camera (0 is usually built-in webcam). """
+        self.cam = cv2.VideoCapture(cam_number)
         while True:
             ret, frame = self.cam.read()
 
             if not ret:
-                print("Failed to grab frame from camera:", self.cam)
+                print("Failed to grab frame from camera:", cam_number)
                 break
 
             # Update the image to tkinter...
@@ -47,6 +50,7 @@ class VideoGUI:
             self.image_label.update()
 
     def stop(self):
+        """ Stop the video feed and destroy all windows. """
         self.cam.release()
         cv2.destroyAllWindows()
         print("Stopped!")
