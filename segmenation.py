4fc49104b717ae768e0a1bc24ec4fd9a2aa74b3e
Lars_Surface <larsgudjons@gmail.com>
2022-12-16 14:56:29 +0100

    Did some cleaning of repo.

--- segmenation.py
+++ segmenation.py
@@ -6,7 +6,6 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import gui_slider
-import countour_detection
 
 # cap = cv2.VideoCapture(2)
 # use folder of images instead of webcam
--- utils_own.py
+++ utils_own.py
@@ -1,6 +1,5 @@
 import cv2
 import os
-import ContourUtils
 import csv
 import numpy as np
 from datetime import datetime
@@ -65,44 +64,6 @@ def undistortFunction(img,mtx,dist):
     dst = dst[y:y+h, x:x+w]
     #cv2.imwrite('calibresult.png',dst)
     return dst
-
-def calculatePixelsPerMetric(img,reorderd,ArucoSize,draw = True):
-    """
-    Calculates the pixels/mm in a given image with an AruCo marker (in the plane of the marker)
-    :param img: image with AruCo marker
-    :param reorderd: the reordered corner points of the marker
-    :param ArucoSize: the size of the marker in mm
-    :param draw: bool if the individual pixels/mm for x and y direction should be drawn on screen
-    :return: returns the average of pixels/mm in x and y direction
-    """
-
-    (tltrX, tltrY) = ContourUtils.midpoint(reorderd[0], reorderd[1])  # top left,top right
-    (blbrX, blbrY) = ContourUtils.midpoint(reorderd[2], reorderd[3])  # bottom left, botto right
-    (tlblX, tlblY) = ContourUtils.midpoint(reorderd[0], reorderd[2])
-    (trbrX, trbrY) = ContourUtils.midpoint(reorderd[1], reorderd[3])
-    if draw:
-        cv2.circle(img, (int(tltrX), int(tltrY)), 1, (255, 255, 0), 2)
-        cv2.circle(img, (int(blbrX), int(blbrY)), 1, (255, 255, 0), 2)
-        cv2.circle(img, (int(tlblX), int(tlblY)), 1, (255, 255, 0), 2)
-        cv2.circle(img, (int(trbrX), int(trbrY)), 1, (255, 255, 0), 2)
-        cv2.line(img, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),  # Draws Lines in Center
-                 (255, 0, 255), 2)
-        cv2.line(img, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
-                 (255, 0, 255), 2)
-
-    dY = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
-    dX = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
-    pixelsPerMetric = (dX + dY) / 2 * (1 / ArucoSize)  # Calculates Pixels/Length Parameter
-    dimA = dY / pixelsPerMetric
-    dimB = dX / pixelsPerMetric  # Dimention of Marker
-    if draw:
-        cv2.putText(img, "{:.1f}mm".format(dimA),
-                    (int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
-                    0.65, (0, 0, 255), 2)
-        cv2.putText(img, "{:.1f}mm".format(dimB),
-                    (int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
-                    0.65, (0, 0, 255), 2)
-    return pixelsPerMetric
 
 
 
