a3d7d7465bffd328114e6d8a60cb44b7fb63ebe2
Lars_Surface <larsgudjons@gmail.com>
2023-01-19 10:29:30 +0100

    Added the tensorrt dependencies.

--- models/common.py
+++ models/common.py
@@ -347,45 +347,45 @@ class DetectMultiBackend(nn.Module):
             names = model.module.names if hasattr(model, 'module') else model.names  # get class names
             model.half() if fp16 else model.float()
             self.model = model  # explicitly assign for to(), cpu(), cuda(), half()
-        elif jit:  # TorchScript
-            LOGGER.info(f'Loading {w} for TorchScript inference...')
-            extra_files = {'config.txt': ''}  # model metadata
-            model = torch.jit.load(w, _extra_files=extra_files, map_location=device)
-            model.half() if fp16 else model.float()
-            if extra_files['config.txt']:  # load metadata dict
-                d = json.loads(extra_files['config.txt'],
-                               object_hook=lambda d: {int(k) if k.isdigit() else k: v
-                                                      for k, v in d.items()})
-                stride, names = int(d['stride']), d['names']
-        elif dnn:  # ONNX OpenCV DNN
-            LOGGER.info(f'Loading {w} for ONNX OpenCV DNN inference...')
-            check_requirements('opencv-python>=4.5.4')
-            net = cv2.dnn.readNetFromONNX(w)
-        elif onnx:  # ONNX Runtime
-            LOGGER.info(f'Loading {w} for ONNX Runtime inference...')
-            check_requirements(('onnx', 'onnxruntime-gpu' if cuda else 'onnxruntime'))
-            import onnxruntime
-            providers = ['CUDAExecutionProvider', 'CPUExecutionProvider'] if cuda else ['CPUExecutionProvider']
-            session = onnxruntime.InferenceSession(w, providers=providers)
-            output_names = [x.name for x in session.get_outputs()]
-            meta = session.get_modelmeta().custom_metadata_map  # metadata
-            if 'stride' in meta:
-                stride, names = int(meta['stride']), eval(meta['names'])
-        elif xml:  # OpenVINO
-            LOGGER.info(f'Loading {w} for OpenVINO inference...')
-            check_requirements('openvino')  # requires openvino-dev: https://pypi.org/project/openvino-dev/
-            from openvino.runtime import Core, Layout, get_batch
-            ie = Core()
-            if not Path(w).is_file():  # if not *.xml
-                w = next(Path(w).glob('*.xml'))  # get *.xml file from *_openvino_model dir
-            network = ie.read_model(model=w, weights=Path(w).with_suffix('.bin'))
-            if network.get_parameters()[0].get_layout().empty:
-                network.get_parameters()[0].set_layout(Layout("NCHW"))
-            batch_dim = get_batch(network)
-            if batch_dim.is_static:
-                batch_size = batch_dim.get_length()
-            executable_network = ie.compile_model(network, device_name="CPU")  # device_name="MYRIAD" for Intel NCS2
-            stride, names = self._load_metadata(Path(w).with_suffix('.yaml'))  # load metadata
+        # elif jit:  # TorchScript
+        #     LOGGER.info(f'Loading {w} for TorchScript inference...')
+        #     extra_files = {'config.txt': ''}  # model metadata
+        #     model = torch.jit.load(w, _extra_files=extra_files, map_location=device)
+        #     model.half() if fp16 else model.float()
+        #     if extra_files['config.txt']:  # load metadata dict
+        #         d = json.loads(extra_files['config.txt'],
+        #                        object_hook=lambda d: {int(k) if k.isdigit() else k: v
+        #                                               for k, v in d.items()})
+        #         stride, names = int(d['stride']), d['names']
+        # elif dnn:  # ONNX OpenCV DNN
+        #     LOGGER.info(f'Loading {w} for ONNX OpenCV DNN inference...')
+        #     check_requirements('opencv-python>=4.5.4')
+        #     net = cv2.dnn.readNetFromONNX(w)
+        # elif onnx:  # ONNX Runtime
+        #     LOGGER.info(f'Loading {w} for ONNX Runtime inference...')
+        #     check_requirements(('onnx', 'onnxruntime-gpu' if cuda else 'onnxruntime'))
+        #     import onnxruntime
+        #     providers = ['CUDAExecutionProvider', 'CPUExecutionProvider'] if cuda else ['CPUExecutionProvider']
+        #     session = onnxruntime.InferenceSession(w, providers=providers)
+        #     output_names = [x.name for x in session.get_outputs()]
+        #     meta = session.get_modelmeta().custom_metadata_map  # metadata
+        #     if 'stride' in meta:
+        #         stride, names = int(meta['stride']), eval(meta['names'])
+        # elif xml:  # OpenVINO
+        #     LOGGER.info(f'Loading {w} for OpenVINO inference...')
+        #     check_requirements('openvino')  # requires openvino-dev: https://pypi.org/project/openvino-dev/
+        #     from openvino.runtime import Core, Layout, get_batch
+        #     ie = Core()
+        #     if not Path(w).is_file():  # if not *.xml
+        #         w = next(Path(w).glob('*.xml'))  # get *.xml file from *_openvino_model dir
+        #     network = ie.read_model(model=w, weights=Path(w).with_suffix('.bin'))
+        #     if network.get_parameters()[0].get_layout().empty:
+        #         network.get_parameters()[0].set_layout(Layout("NCHW"))
+        #     batch_dim = get_batch(network)
+        #     if batch_dim.is_static:
+        #         batch_size = batch_dim.get_length()
+        #     executable_network = ie.compile_model(network, device_name="CPU")  # device_name="MYRIAD" for Intel NCS2
+        #     stride, names = self._load_metadata(Path(w).with_suffix('.yaml'))  # load metadata
         elif engine:  # TensorRT
             LOGGER.info(f'Loading {w} for TensorRT inference...')
             import tensorrt as trt  # https://developer.nvidia.com/nvidia-tensorrt-download
@@ -417,81 +417,81 @@ class DetectMultiBackend(nn.Module):
                 bindings[name] = Binding(name, dtype, shape, im, int(im.data_ptr()))
             binding_addrs = OrderedDict((n, d.ptr) for n, d in bindings.items())
             batch_size = bindings['images'].shape[0]  # if dynamic, this is instead max batch size
-        elif coreml:  # CoreML
-            LOGGER.info(f'Loading {w} for CoreML inference...')
-            import coremltools as ct
-            model = ct.models.MLModel(w)
-        elif saved_model:  # TF SavedModel
-            LOGGER.info(f'Loading {w} for TensorFlow SavedModel inference...')
-            import tensorflow as tf
-            keras = False  # assume TF1 saved_model
-            model = tf.keras.models.load_model(w) if keras else tf.saved_model.load(w)
-        elif pb:  # GraphDef https://www.tensorflow.org/guide/migrate#a_graphpb_or_graphpbtxt
-            LOGGER.info(f'Loading {w} for TensorFlow GraphDef inference...')
-            import tensorflow as tf
-
-            def wrap_frozen_graph(gd, inputs, outputs):
-                x = tf.compat.v1.wrap_function(lambda: tf.compat.v1.import_graph_def(gd, name=""), [])  # wrapped
-                ge = x.graph.as_graph_element
-                return x.prune(tf.nest.map_structure(ge, inputs), tf.nest.map_structure(ge, outputs))
-
-            def gd_outputs(gd):
-                name_list, input_list = [], []
-                for node in gd.node:  # tensorflow.core.framework.node_def_pb2.NodeDef
-                    name_list.append(node.name)
-                    input_list.extend(node.input)
-                return sorted(f'{x}:0' for x in list(set(name_list) - set(input_list)) if not x.startswith('NoOp'))
-
-            gd = tf.Graph().as_graph_def()  # TF GraphDef
-            with open(w, 'rb') as f:
-                gd.ParseFromString(f.read())
-            frozen_func = wrap_frozen_graph(gd, inputs="x:0", outputs=gd_outputs(gd))
-        elif tflite or edgetpu:  # https://www.tensorflow.org/lite/guide/python#install_tensorflow_lite_for_python
-            try:  # https://coral.ai/docs/edgetpu/tflite-python/#update-existing-tf-lite-code-for-the-edge-tpu
-                from tflite_runtime.interpreter import Interpreter, load_delegate
-            except ImportError:
-                import tensorflow as tf
-                Interpreter, load_delegate = tf.lite.Interpreter, tf.lite.experimental.load_delegate,
-            if edgetpu:  # TF Edge TPU https://coral.ai/software/#edgetpu-runtime
-                LOGGER.info(f'Loading {w} for TensorFlow Lite Edge TPU inference...')
-                delegate = {
-                    'Linux': 'libedgetpu.so.1',
-                    'Darwin': 'libedgetpu.1.dylib',
-                    'Windows': 'edgetpu.dll'}[platform.system()]
-                interpreter = Interpreter(model_path=w, experimental_delegates=[load_delegate(delegate)])
-            else:  # TFLite
-                LOGGER.info(f'Loading {w} for TensorFlow Lite inference...')
-                interpreter = Interpreter(model_path=w)  # load TFLite model
-            interpreter.allocate_tensors()  # allocate
-            input_details = interpreter.get_input_details()  # inputs
-            output_details = interpreter.get_output_details()  # outputs
-            # load metadata
-            with contextlib.suppress(zipfile.BadZipFile):
-                with zipfile.ZipFile(w, "r") as model:
-                    meta_file = model.namelist()[0]
-                    meta = ast.literal_eval(model.read(meta_file).decode("utf-8"))
-                    stride, names = int(meta['stride']), meta['names']
-        elif tfjs:  # TF.js
-            raise NotImplementedError('ERROR: YOLOv5 TF.js inference is not supported')
-        elif paddle:  # PaddlePaddle
-            LOGGER.info(f'Loading {w} for PaddlePaddle inference...')
-            check_requirements('paddlepaddle-gpu' if cuda else 'paddlepaddle')
-            import paddle.inference as pdi
-            if not Path(w).is_file():  # if not *.pdmodel
-                w = next(Path(w).rglob('*.pdmodel'))  # get *.pdmodel file from *_paddle_model dir
-            weights = Path(w).with_suffix('.pdiparams')
-            config = pdi.Config(str(w), str(weights))
-            if cuda:
-                config.enable_use_gpu(memory_pool_init_size_mb=2048, device_id=0)
-            predictor = pdi.create_predictor(config)
-            input_handle = predictor.get_input_handle(predictor.get_input_names()[0])
-            output_names = predictor.get_output_names()
-        elif triton:  # NVIDIA Triton Inference Server
-            LOGGER.info(f'Using {w} as Triton Inference Server...')
-            check_requirements('tritonclient[all]')
-            from utils.triton import TritonRemoteModel
-            model = TritonRemoteModel(url=w)
-            nhwc = model.runtime.startswith("tensorflow")
+        # elif coreml:  # CoreML
+        #     LOGGER.info(f'Loading {w} for CoreML inference...')
+        #     import coremltools as ct
+        #     model = ct.models.MLModel(w)
+        # elif saved_model:  # TF SavedModel
+        #     LOGGER.info(f'Loading {w} for TensorFlow SavedModel inference...')
+        #     import tensorflow as tf
+        #     keras = False  # assume TF1 saved_model
+        #     model = tf.keras.models.load_model(w) if keras else tf.saved_model.load(w)
+        # elif pb:  # GraphDef https://www.tensorflow.org/guide/migrate#a_graphpb_or_graphpbtxt
+        #     LOGGER.info(f'Loading {w} for TensorFlow GraphDef inference...')
+        #     import tensorflow as tf
+        #
+        #     def wrap_frozen_graph(gd, inputs, outputs):
+        #         x = tf.compat.v1.wrap_function(lambda: tf.compat.v1.import_graph_def(gd, name=""), [])  # wrapped
+        #         ge = x.graph.as_graph_element
+        #         return x.prune(tf.nest.map_structure(ge, inputs), tf.nest.map_structure(ge, outputs))
+        #
+        #     def gd_outputs(gd):
+        #         name_list, input_list = [], []
+        #         for node in gd.node:  # tensorflow.core.framework.node_def_pb2.NodeDef
+        #             name_list.append(node.name)
+        #             input_list.extend(node.input)
+        #         return sorted(f'{x}:0' for x in list(set(name_list) - set(input_list)) if not x.startswith('NoOp'))
+        #
+        #     gd = tf.Graph().as_graph_def()  # TF GraphDef
+        #     with open(w, 'rb') as f:
+        #         gd.ParseFromString(f.read())
+        #     frozen_func = wrap_frozen_graph(gd, inputs="x:0", outputs=gd_outputs(gd))
+        # elif tflite or edgetpu:  # https://www.tensorflow.org/lite/guide/python#install_tensorflow_lite_for_python
+        #     try:  # https://coral.ai/docs/edgetpu/tflite-python/#update-existing-tf-lite-code-for-the-edge-tpu
+        #         from tflite_runtime.interpreter import Interpreter, load_delegate
+        #     except ImportError:
+        #         import tensorflow as tf
+        #         Interpreter, load_delegate = tf.lite.Interpreter, tf.lite.experimental.load_delegate,
+        #     if edgetpu:  # TF Edge TPU https://coral.ai/software/#edgetpu-runtime
+        #         LOGGER.info(f'Loading {w} for TensorFlow Lite Edge TPU inference...')
+        #         delegate = {
+        #             'Linux': 'libedgetpu.so.1',
+        #             'Darwin': 'libedgetpu.1.dylib',
+        #             'Windows': 'edgetpu.dll'}[platform.system()]
+        #         interpreter = Interpreter(model_path=w, experimental_delegates=[load_delegate(delegate)])
+        #     else:  # TFLite
+        #         LOGGER.info(f'Loading {w} for TensorFlow Lite inference...')
+        #         interpreter = Interpreter(model_path=w)  # load TFLite model
+        #     interpreter.allocate_tensors()  # allocate
+        #     input_details = interpreter.get_input_details()  # inputs
+        #     output_details = interpreter.get_output_details()  # outputs
+        #     # load metadata
+        #     with contextlib.suppress(zipfile.BadZipFile):
+        #         with zipfile.ZipFile(w, "r") as model:
+        #             meta_file = model.namelist()[0]
+        #             meta = ast.literal_eval(model.read(meta_file).decode("utf-8"))
+        #             stride, names = int(meta['stride']), meta['names']
+        # elif tfjs:  # TF.js
+        #     raise NotImplementedError('ERROR: YOLOv5 TF.js inference is not supported')
+        # elif paddle:  # PaddlePaddle
+        #     LOGGER.info(f'Loading {w} for PaddlePaddle inference...')
+        #     check_requirements('paddlepaddle-gpu' if cuda else 'paddlepaddle')
+        #     import paddle.inference as pdi
+        #     if not Path(w).is_file():  # if not *.pdmodel
+        #         w = next(Path(w).rglob('*.pdmodel'))  # get *.pdmodel file from *_paddle_model dir
+        #     weights = Path(w).with_suffix('.pdiparams')
+        #     config = pdi.Config(str(w), str(weights))
+        #     if cuda:
+        #         config.enable_use_gpu(memory_pool_init_size_mb=2048, device_id=0)
+        #     predictor = pdi.create_predictor(config)
+        #     input_handle = predictor.get_input_handle(predictor.get_input_names()[0])
+        #     output_names = predictor.get_output_names()
+        # elif triton:  # NVIDIA Triton Inference Server
+        #     LOGGER.info(f'Using {w} as Triton Inference Server...')
+        #     check_requirements('tritonclient[all]')
+        #     from utils.triton import TritonRemoteModel
+        #     model = TritonRemoteModel(url=w)
+        #     nhwc = model.runtime.startswith("tensorflow")
         else:
             raise NotImplementedError(f'ERROR: {w} is not a supported format')
 
@@ -596,20 +596,20 @@ class DetectMultiBackend(nn.Module):
             for _ in range(2 if self.jit else 1):  #
                 self.forward(im)  # warmup
 
-    @staticmethod
-    def _model_type(p='path/to/model.pt'):
-        # Return model type from model path, i.e. path='path/to/model.onnx' -> type=onnx
-        # types = [pt, jit, onnx, xml, engine, coreml, saved_model, pb, tflite, edgetpu, tfjs, paddle]
-        from export import export_formats
-        from utils.downloads import is_url
-        sf = list(export_formats().Suffix)  # export suffixes
-        if not is_url(p, check=False):
-            check_suffix(p, sf)  # checks
-        url = urlparse(p)  # if url may be Triton inference server
-        types = [s in Path(p).name for s in sf]
-        types[8] &= not types[9]  # tflite &= not edgetpu
-        triton = not any(types) and all([any(s in url.scheme for s in ["http", "grpc"]), url.netloc])
-        return types + [triton]
+    # @staticmethod
+    # def _model_type(p='path/to/model.pt'):
+    #     # Return model type from model path, i.e. path='path/to/model.onnx' -> type=onnx
+    #     # types = [pt, jit, onnx, xml, engine, coreml, saved_model, pb, tflite, edgetpu, tfjs, paddle]
+    #     from export import export_formats
+    #     from utils.downloads import is_url
+    #     sf = list(export_formats().Suffix)  # export suffixes
+    #     if not is_url(p, check=False):
+    #         check_suffix(p, sf)  # checks
+    #     url = urlparse(p)  # if url may be Triton inference server
+    #     types = [s in Path(p).name for s in sf]
+    #     types[8] &= not types[9]  # tflite &= not edgetpu
+    #     triton = not any(types) and all([any(s in url.scheme for s in ["http", "grpc"]), url.netloc])
+    #     return types + [triton]
 
     @staticmethod
     def _load_metadata(f=Path('path/to/meta.yaml')):
